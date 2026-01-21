7ad08f35895d4790b998d1af750ff6c3ff9f187a
always <alwaysstrive2024@gmail.com>
2026-01-21 14:10:20 -0600

    guided

--- test.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-# Load the e-SNLI dataset
-dataset = load_dataset("esnli", trust_remote_code=True)
-
-train_dataset = dataset['train']
-eval_dataset = dataset['validation']
